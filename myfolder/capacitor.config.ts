2b0357a8e11e4f6526eb761b24946c5c86e08c30
Jaga234-hue <jaga232@gmail.com>
2026-02-15 21:14:52 +0530

    Rename folder from myfolder to frontend

--- myfolder/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.aiva.navigator',
-  appName: 'AIVA',
-  webDir: 'build'
-};
-
-export default config;
